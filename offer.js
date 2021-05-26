45dee187710a31c36c32b1f5374f90e9e31becba
mmitou <masayuki.itou.work@gmail.com>
2021-05-26 16:12:29 +0900

    init

---
+++ offer.js
@@ -0,0 +1,83 @@
+"use strict";
+
+let localVideoStream = null;
+let conn = null;
+
+document
+	.getElementById("localVideoButton")
+	.addEventListener("click", async (event) => {
+		const localVideo = document.getElementById("localVideo");
+		if (localVideoStream) {
+			localVideo.srcObject.getTracks().forEach((track) => {
+				track.stop();
+			});
+			localVideoStream = null;
+			localVideo.srcObject = null;
+		} else {
+			localVideoStream = await navigator.mediaDevices.getUserMedia({
+				video: true,
+				audio: false,
+			});
+			localVideo.srcObject = localVideoStream;
+		}
+	});
+
+document
+	.getElementById("createOfferButton")
+	.addEventListener("click", async (event) => {
+		conn = new RTCPeerConnection({
+			iceServers: [
+				{
+					urls: [
+						"stun:stun.l.google.com:19302",
+						"stun:stun1.l.google.com:19302",
+						"stun:stun2.l.google.com:19302",
+						"stun:stun3.l.google.com:19302",
+						"stun:stun4.l.google.com:19302",
+					],
+				},
+			],
+		});
+		conn.addEventListener("connectionstatechange", console.log);
+		conn.addEventListener("icecandidate", (event) => {
+			console.log("icecandidate", event.candidate);
+			const d = document.createElement("div");
+			d.textContent = JSON.stringify(event.candidate);
+			document.getElementById("icecandidates").appendChild(d);
+		});
+		conn.addEventListener("icecandidateerror", console.log);
+		conn.addEventListener("iceconnectionstatechange", console.log);
+		conn.addEventListener("icegatheringstatechange", console.log);
+		conn.addEventListener("negotiationneeded", console.log);
+		conn.addEventListener("statsended", console.log);
+		conn.addEventListener("track", (event) => {
+			const stream = event.streams[0];
+			document.getElementById("remoteVideo").srcObject = stream;
+		});
+
+		localVideoStream.getTracks().forEach((track) => {
+			conn.addTrack(track, localVideoStream);
+		});
+
+		const offer = await conn.createOffer();
+		await conn.setLocalDescription(offer);
+
+		console.log("sdp offer", offer);
+		document.getElementById("sdpOffer").value = JSON.stringify(offer);
+	});
+
+document
+	.getElementById("recieveAnswerButton")
+	.addEventListener("click", async (event) => {
+		const txt = document.getElementById("sdpAnswer").value;
+		const answer = JSON.parse(txt);
+		await conn.setRemoteDescription(answer);
+	});
+
+document
+	.getElementById("recieveCandidateButton")
+	.addEventListener("click", async (event) => {
+		const txt = document.getElementById("candidate").value;
+		const candidate = JSON.parse(txt);
+		await conn.addIceCandidate(candidate);
+	});
